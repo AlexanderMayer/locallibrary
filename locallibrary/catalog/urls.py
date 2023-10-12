884095185fab57936c29ab94eb696f8181ad1b14
gr423_mease <sanymini@gmail.com>
2023-10-12 13:18:00 +0700

    начало 8 задания

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,16 +1,10 @@
 from django.urls import path
 from . import views
-from django.conf.urls import url
-from django.views import generic
+from django.urls import include
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^books/$', views.BookListView.as_view(), name='books'),
-    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
-    url(r'^/url/$', views.my_reused_view, {'my_template_name': 'some_path'}, name='aurl'),
-    url(r'^/anotherurl/$', views.my_reused_view, {'my_template_name': 'another_path'}, name='anotherurl'),
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
 
-
-class BookListView(generic.ListView):
-    model = Book
--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -6,10 +6,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
     path('catalog/', include('catalog.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
